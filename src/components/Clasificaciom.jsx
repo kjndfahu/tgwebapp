b1663f32ca15afc0f093e52c921a78164af79644
kjndfahu <denispoda59@gmail.com>
2024-09-29 15:22:41 +0300

    full proj

--- src/components/Clasificaciom.jsx
+++ src/components/Clasificaciom.jsx
@@ -7,51 +7,61 @@ function Clasificaciom() {
     const data = [
         {
             id: 1,
+            rankColor: 'bg-[#e2c317]',
             name: "Henry",
             value: "17M"
         },
         {
             id: 2,
+            rankColor: 'bg-[#b9bab8]',
             name: "Jegr",
             value: "13M"
         },
         {
             id: 3,
+            rankColor: 'bg-[#e89417]',
             name: "Kevin",
             value: "12M"
         },
         {
             id: 4,
+            rankColor: 'bg-[#353535]',
             name: "Henry",
             value: "11.4M"
         },
         {
             id: 5,
+            rankColor: 'bg-[#353535]',
             name: "Henry",
             value: "10.8M"
         },
         {
             id: 6,
+            rankColor: 'bg-[#353535]',
             name: "Henry",
             value: "10M"
         },
         {
             id: 7,
+            rankColor: 'bg-[#353535]',
             name: "Henry",
             value: "9.6M"
         },
         {
             id: 8,
+            rankColor: 'bg-[#353535]',
             name: "Henry",
             value: "9.2M"
         },
         {
             id: 9,
+            rankColor: 'bg-[#353535]',
             name: "Henry",
             value: "8.9M"
         },
         {
             id: 10,
+            rankColor: 'bg-[#353535]',
             name: "Henry",
             value: "7.2M"
         },
@@ -64,7 +74,7 @@ function Clasificaciom() {
                 <h2 className="font-sfprosemibold text-left text-white text-[27px]">Clasificaciom</h2>
                 <div className="flex items-center px-5 py-3 bg-[#212121] rounded-[10px] justify-between w-[90vw]">
                     <div className="flex flex-row items-center gap-2">
-                        <div className="flex text-white p-1 bg-[#b0b0b0] rounded-[5px] text-[12px]">1k+</div>
+                        <div className="flex text-white p-1 bg-[#353535] rounded-[5px] text-[12px]">1k+</div>
                         <div className="bg-[#b0b0b0] w-[40px] h-[40px] rounded-full"></div>
                         <h2 className="text-white text-[18px] font-sfpromedium">bell</h2>
                     </div>
@@ -75,12 +85,12 @@ function Clasificaciom() {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col mt-7 gap-3">
+            <div className="flex flex-col mt-7 mb-[50px] gap-3">
                 <h2 className="font-sfprosemibold text-left text-white text-[27px]">Jugadores top</h2>
                 {data.map((item) => (
                     <div className="flex items-center px-5 py-3 bg-[#212121] rounded-[10px] justify-between w-[90vw]">
                         <div className="flex flex-row items-center gap-2">
-                            <div className="flex text-white py-1 px-3 bg-[#b0b0b0] rounded-[5px] text-[12px]">{item.id}</div>
+                            <div className={`flex text-white py-1 px-3 rounded-[5px] text-[12px] ${item.rankColor}`}>{item.id}</div>
                             <div className="bg-[#b0b0b0] w-[40px] h-[40px] rounded-full"></div>
                             <h2 className="text-white text-[18px] font-sfpromedium">{item.name}</h2>
                         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,10 +9,12 @@ import {Route} from "react-router-dom";
 function Home({isActive, setActive}) {
     return (
         <Suspense fallback={ <p className="text-black text-[96px]">Loading..</p> }>
-            <Header/>
-            <CounterTitul/>
-            <ProgressBar isActive={isActive} setActive={setActive} />
-            {isActive && <ModalEnergiaExtra isActive={isActive} setActive={setActive} /> }
+            <div className="flex flex-col h-[90%] justify-between">
+                <Header/>
+                <CounterTitul/>
+                <ProgressBar isActive={isActive} setActive={setActive} />
+                {isActive && <ModalEnergiaExtra isActive={isActive} setActive={setActive} /> }
+            </div>
         </Suspense>
     )
 }
