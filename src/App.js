f41e1b00b8176c2e9784c61aca0f45066cb75bb1
kjndfahu <denispoda59@gmail.com>
2024-10-02 15:12:39 +0300

    weww

--- src/App.js
+++ src/App.js
@@ -12,6 +12,24 @@ function App() {
     window.Telegram.WebApp.expand();
     window.Telegram.WebApp.disableVerticalSwipes()
 
+    useEffect(() => {
+        // Функция для блокировки прокрутки
+        const preventDefault = (e) => {
+            e.preventDefault();
+        };
+
+        // Добавляем обработчики событий
+        window.addEventListener('wheel', preventDefault, { passive: false });
+        window.addEventListener('touchmove', preventDefault, { passive: false });
+        document.body.style.overflow = 'hidden'; // Отключает прокрутку
+
+        // Убираем обработчики при размонтировании
+        return () => {
+            window.removeEventListener('wheel', preventDefault);
+            window.removeEventListener('touchmove', preventDefault);
+            document.body.style.overflow = ''; // Включает прокрутку обратно
+        };
+    }, []);
 
     return (
         <div className="flex flex-col overflow-x-hidden overflow-y-hidden items-center text-center gap-4 ">
