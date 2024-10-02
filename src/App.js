c96e33ccbd42af2824cc88fbefd8fa3ca0b64211
kjndfahu <denispoda59@gmail.com>
2024-10-02 13:59:08 +0300

    wwdc

--- src/App.js
+++ src/App.js
@@ -10,12 +10,7 @@ import MainWindow from "./components/MainWindow";
 function App() {
     window.Telegram.WebApp.ready();
     window.Telegram.WebApp.expand();
-    const tg = window.Telegram.WebApp;
 
-    useEffect(() => {
-        // Отключаем скроллинг
-        tg.setScrollable(false);
-    }, []);
     return (
         <div className="flex flex-col overflow-x-hidden overflow-y-hidden items-center text-center gap-4 h-[100vh]">
             <MainWindow/>
