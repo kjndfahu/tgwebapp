d68b91fcf331081fb3a95e6865d933b1a814206a
kjndfahu <denispoda59@gmail.com>
2024-10-02 15:31:09 +0300

    wwew

--- src/components/Clasificaciom.jsx
+++ src/components/Clasificaciom.jsx
@@ -58,7 +58,7 @@ function Clasificaciom({setIsScrollEnabled}) {
 
 
     return (
-        <div className="flex flex-col items-center bg-[url('https://i.imgur.com/IDlQwiO.png')] w-full h-[100vh] ">
+        <div className="flex flex-col items-center bg-[url('https://i.imgur.com/IDlQwiO.png')] w-full h-[100%] mb-24">
             <div className="flex flex-col mt-6 gap-3">
                 <h2 className="font-sfprosemibold text-left text-white text-[27px]">Clasificaciom</h2>
 
