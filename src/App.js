a60887a3101d028138713e8c51d658d4a23cff5c
kjndfahu <denispoda59@gmail.com>
2024-10-02 14:52:26 +0300

    sdwsdww

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
 
     return (
-        <div className="flex flex-col overflow-x-hidden overflow-y-hidden items-center text-center gap-4 h-[100vh]">
+        <div className="flex flex-col overflow-x-hidden overflow-y-hidden items-center text-center gap-4 ">
             <MainWindow/>
         </div>)
 }
