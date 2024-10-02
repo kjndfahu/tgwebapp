9f8b0d322c4914e754606cdec25be7547078e8c0
kjndfahu <denispoda59@gmail.com>
2024-10-02 13:06:44 +0300

    wweeppp

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import MainWindow from "./components/MainWindow";
 
 function App() {
     return (
-        <div className="flex flex-col overflow-x-hidden items-center text-center gap-4 h-[100vh]">
+        <div className="flex flex-col overflow-x-hidden overflow-y-hidden items-center text-center gap-4 h-[100vh]">
             <MainWindow/>
         </div>)
 }
